e675044376eecb7ab883eb0b6677ac2a3fd6d593
Karl Hänsel <Karl-Haensel@gmx.de>
2026-03-28 21:47:00 +0100

    Establish first minimal project setup.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sabam
-Look at, measure, and draw cool things with interacitve app.
+Look at, measure, and draw cool things with interactive map.
+
+This is supposed to be a PWA (Progressive Web App).
+So it's time to say: GO, GO, GOOO!
+
+## Third-Party Credits
+The app icon is by Selman Design and under CC BY 4.0 license (see https://creativecommons.org/licenses/by/4.0/). It can be found here: https://icon-icons.com/de/symbol/anzeigen/126331 (as of 2026-03-28). No changes were made.
---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("my-cache").then((cache) => {
+      return cache.addAll([
+        "index.html",
+        "script.js",
+        "styles.css",
+        "icon.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((response) => {
+        const cache = caches.open("my-cache");
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    })
+  );
+});
