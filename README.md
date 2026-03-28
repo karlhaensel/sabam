f27709962b662d12ea04eff58929a671548f8ff2
Karl Hänsel <Karl-Haensel@gmx.de>
2026-03-29 00:09:38 +0100

    Create first map with a few elements to test leaflet.

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 Look at, measure, and draw cool things with interactive map.
 
 This is supposed to be a PWA (Progressive Web App).
+
 So it's time to say: GO, GO, GOOO!
 
-## Third-Party Credits
-The app icon is by Selman Design and under CC BY 4.0 license (see https://creativecommons.org/licenses/by/4.0/). It can be found here: https://icon-icons.com/de/symbol/anzeigen/126331 (as of 2026-03-28). No changes were made.
+## Develop/test the current version
+1. clone this repo
+2. start local web server
+3. run locally in browser
+4. debug with devtools (F12)
+
+For example, if you are using `vscode`, install the extension "Live Server", right-click on `index.html` and choose "Open with Live Server".
+
+## Credits
+We are using an [icon by Selman Design](https://icon-icons.com/de/symbol/anzeigen/126331) and [leaflet.js](https://leafletjs.com). See more in [Credits.md](https://github.com/karlhaensel/sabam/blob/main/CREDITS.md).
--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("sw.js")
-    .then(() => console.log("Service Worker registered"))
-    .catch((err) => console.error("Service Worker registration failed:", err));
+// Setup Service Worker (preparation for later on, left out for now)
+// if ("serviceWorker" in navigator) {
+//   navigator.serviceWorker.register("sw.js")
+//     .then(() => console.log("Service Worker registered"))
+//     .catch((err) => console.error("Service Worker registration failed:", err));
+// }
+
+// Setup test map (Hamburg, meine Perle)
+var popup = L.popup();
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("Oh, <b>beautiful</b> choice here at " + e.latlng.toString())
+    .openOn(map);
 }
+
+const map = L.map("map").setView([53.550556, 9.993333], 12);
+map.on("click", onMapClick);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+const GEOMATIKUM_COORD = [53.56827250333696, 9.974483470522147];
+const geomatikumMarker = L.marker(GEOMATIKUM_COORD).addTo(map);
+geomatikumMarker.bindPopup("GEOMATIKUM").openPopup();
+const geomatikumZone = L.circle([53.56827250333696, 9.974483470522147], {
+  color: "red",
+  fillColor: "rgb(59, 129, 32)",
+  fillOpacity: 0.5,
+  radius: 500, // metres
+}).addTo(map);
+geomatikumZone.bindPopup("Geomatikum <b>zone</b>");
+
+const START_COORD = [53.55376219840088, 9.991941297506065];
+const startPopup = L.popup()
+  .setLatLng(START_COORD)
+  .setContent("GO! GO! GOOO!")
+  .openOn(map);
